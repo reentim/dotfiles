4faef1d45499296f7aa44da875dc281edf670047
Timothy Reen <reentim@gmail.com>
2012-07-19 14:17:38 +1000

    Added uninstallation script.

---
+++ uninstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+bold=`tput bold`
+normal=`tput sgr0`
+
+echo -e "Removing linked dotfiles..."
+# vim
+	rm ~/.vim
+	rm ~/.vimrc
+	rm ~/.gvimrc
+# bash
+	rm ~/.bash_aliases
+	rm ~/.bash_custom
+	rm ~/.bash_colors
+	rm ~/.bash_profile
+# git 
+	rm ~/.gitconfig
+	rm ~/.githelpers
+	rm ~/.gitignore-global
+echo -e "Restoring backed-up vim dotfiles..."
+	mv ~/.backup-dotfiles/* ~/
+	rmdir ~/.backup-dotfiles
+
+echo -e "${bold}Uninstallation complete: original setup restored!${normal}\n"
