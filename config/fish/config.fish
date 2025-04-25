47a2ea7ed9d50a0fade92713b32856727975feed
Timothy Reen <reentim@gmail.com>
2025-04-25 13:03:47 +1000

    Give fish shell a try

---
+++ config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+  # fzf --fish | source
+  # fisher update
+end
+
+# function fish_user_key_bindings
+#   bind -M insert \cr __fzf_search_history
+#   bind -M default \cr __fzf_search_history
+# end
